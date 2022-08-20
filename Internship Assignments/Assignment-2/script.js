f3dd5034b1aa5b2502cd154b9b303b65c32ed622
Aayush Rana <69390908+TheAayushRana@users.noreply.github.com>
2022-08-20 16:56:18 +0530

    Updated Assigment

--- Internship Assignments/Assignment-2/script.js
+++ Internship Assignments/Assignment-2/script.js
@@ -1,14 +1,26 @@
 import { data } from "./data.js";
 
-var table = document.getElementById("tabledata");
-var header = table.createTHead();
-var body = table.createTBody();
-for (let i = 0; i < data.length; i++) {
+let table = document.getElementById("tabledata");
+let header = table.createTHead();
+let body = table.createTBody();
+
+for (let i = 0; i < data.length + 1; i++) {
   // to create new rows
-  var newrow = i < 1 ? header.insertRow(i) : body.insertRow(i - 1);
-  for (let j = 0; j < data[i].length; j++) {
+  let newrow = i < 2 ? header.insertRow(i) : body.insertRow(i - 2);
+  let newi = i == 0 || i == 1 ? 0 : i - 1;
+  for (let j = 0; j < data[newi].length; j++) {
     // to create new cells
-    var newcell = newrow.insertCell(j);
-    newcell.innerHTML = data[i][j];
+    let newcell = newrow.insertCell(j);
+    if (i == 0 && j == 8) {
+      newcell.colSpan = "3";
+      newcell.innerHTML = "Energy Consumed";
+    } else if (i == 1) {
+      newcell.innerHTML = " ";
+      if (j == 8 || j == 9 || j == 10) {
+        newcell.innerHTML = data[0][j];
+      }
+    } else {
+      newcell.innerHTML = data[newi][j];
+    }
   }
 }
